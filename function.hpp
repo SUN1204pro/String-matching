ba1fc808dde2f6f69cbe2637a7c59869ef551ec5
Sunix1204pro <phamminhnhat1204@gmail.com>
2026-03-26 11:35:30 +0700

    naive and aho

---
+++ function.hpp
@@ -0,0 +1,7 @@
+
+
+#ifndef function.hpp
+#define function.hpp
+void naive(const vector<vector<char>>,string);
+void Aho_Corasick(const vector<vector<char>>& grid, TrieNode* root, const vector<string>& queries);
+#endif function.hpp
