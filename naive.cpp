6366334bd4a9a42d08e332ece6684f092b660f04
Sunix1204pro <phamminhnhat1204@gmail.com>
2026-03-28 12:10:21 +0700

    update new

--- naive.cpp
+++ naive.cpp
@@ -4,18 +4,16 @@
 
 using namespace std;
 
-struct Result {
-    int r1, c1, r2, c2;
-};
 
 int dr[] = {0, 0, 1, -1};
 int dc[] = {1, -1, 0, 0};
 
-void naive(const vector<vector<char>> grid,string word) {
+void naive(const vector<vector<char>>& grid, const string& word) {
     int rows = grid.size();
     int cols = grid[0].size();
     int len = word.size();
-    vector<Result> ans;
+
+    vector<string> ans;
     int comparisons = 0;
 
     for (int r = 0; r < rows; ++r) {
@@ -28,12 +26,17 @@ void naive(const vector<vector<char>> grid,string word) {
 
                     if (curr_r < 0 || curr_r >= rows || curr_c < 0 || curr_c >= cols) break;
 
-                    comparisons++; 
+                    comparisons++;
                     if (grid[curr_r][curr_c] != word[k]) break;
                 }
-                
+
                 if (k == len) {
-                    ans.push_back({r, c, r + dr[dir] * (len - 1), c + dc[dir] * (len - 1)});
+                    int end_r = r + dr[dir] * (len - 1);
+                    int end_c = c + dc[dir] * (len - 1);
+
+                    string result = "(" + to_string(r) + ", " + to_string(c) + ") -> " +
+                                              "(" + to_string(end_r) + ", " + to_string(end_c) + ")";
+                    ans.push_back(result);
                 }
             }
         }
@@ -41,27 +44,28 @@ void naive(const vector<vector<char>> grid,string word) {
 
     cout << word << ": ";
     if (ans.empty()) {
-        cout << "not found" << endl;
+        cout << "not found\n";
     } else {
         for (size_t i = 0; i < ans.size(); ++i) {
-            cout << "(" << ans[i].r1 << ", " << ans[i].c1 << ") -> "
-                 << "(" << ans[i].r2 << ", " << ans[i].c2 << ")";
+            cout << ans[i];
             if (i < ans.size() - 1) cout << "; ";
         }
-        cout << ";" << '\n';
-        cout << "Comparisons: " << comparisons << '\n';
+        cout << ";\n";
     }
+    cout << "Comparisons: " << comparisons << '\n';
 }
 
 int main() {
     int n, m;
-    cin>>n>>m;
+    cin >> n >> m;
+
     vector<vector<char>> grid(n, vector<char>(m));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> grid[i][j];
         }
     }
+
     int q;
     cin >> q;
     while (q--) {
