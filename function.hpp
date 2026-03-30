cd6022decd7d2efaa7c08ff30ed935ece91a0af8
Vu Kiet <xirackss@gmail.com>
2026-03-30 12:32:54 +0700

    branch init

--- function.hpp
+++
@@ -1,7 +0,0 @@
-
-
-#ifndef function.hpp
-#define function.hpp
-void naive(const vector<vector<char>>,string);
-void Aho_Corasick(const vector<vector<char>>& grid, TrieNode* root, const vector<string>& queries);
-#endif function.hpp
